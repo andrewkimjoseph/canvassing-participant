8cec3c965972290ea52b4da58b483d67ee158906
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-11-08 17:09:59 +0300

    Use [Link] from Chakra UI

--- front-end/react-app/components/DrawerCardC.tsx
+++ front-end/react-app/components/DrawerCardC.tsx
@@ -1,10 +1,7 @@
 'use client';
 
-import { FC } from 'react';
-import Link from 'next/link';
 import { IconSvgProps } from '@/types/svgIcon';
-import { Box, Text, Card, Icon } from '@chakra-ui/react';
-import { useRouter } from 'next/navigation';
+import { Box, Text, Card, Icon, Link } from '@chakra-ui/react';
 
 interface Props {
   SVGIcon: React.FC<IconSvgProps>;
@@ -13,31 +10,23 @@ interface Props {
 }
 
 export const DrawerCardC: React.FC<Props> = ({ SVGIcon, text, link }) => {
-  const router = useRouter();
-
   return (
-    <Link href={link}>
-      <Box w={'full'} className="flex flex-col" mt={1}>
-        <Card.Root
-          variant={'outline'}
-          borderRadius={12}
-          w={'full'}
-          bgColor={'#363062'}
-        >
-          <Card.Body p={2}>
-            <Box
-              w={'full'}
-              className="flex flex-row items-center justify-between"
-            >
-              <Box className="flex flex-row items-center justify-center">
-                <SVGIcon />
-                <Text fontSize={20} color="white" pl={2}>
-                  {text}
-                </Text>
-              </Box>
-            </Box>
-          </Card.Body>
-        </Card.Root>
+    <Link
+      href={link}
+      _hover={{
+        color: '#363062',
+        textDecoration: 'none',
+        bg: 'white',
+      }}
+      py={2}
+      m={2}
+      rounded={'lg'}
+    >
+      <Box className="flex flex-row items-center justify-center" m={2}>
+        <SVGIcon />
+        <Text fontSize={20} pl={2}>
+          {text}
+        </Text>
       </Box>
     </Link>
   );
