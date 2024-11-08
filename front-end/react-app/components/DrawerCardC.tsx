d480a2cbd82bb65f24b1c8c1a96044efa4b6d423
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-11-08 19:34:08 +0300

    Update card

--- front-end/react-app/components/DrawerCardC.tsx
+++ front-end/react-app/components/DrawerCardC.tsx
@@ -13,18 +13,16 @@ export const DrawerCardC: React.FC<Props> = ({ SVGIcon, text, link }) => {
   return (
     <Link
       href={link}
-      _hover={{
-        color: '#363062',
-        textDecoration: 'none',
-        bg: 'white',
-      }}
       py={2}
-      m={2}
-      rounded={'lg'}
     >
-      <Box className="flex flex-row items-center justify-center" m={2}>
+      <Box
+        className="flex flex-row items-center justify-center"
+        // m={2}
+        my={1}
+        rounded={'lg'}
+      >
         <SVGIcon />
-        <Text fontSize={20} pl={2} color={"white"}>
+        <Text fontSize={20} pl={2} color={'white'}>
           {text}
         </Text>
       </Box>
