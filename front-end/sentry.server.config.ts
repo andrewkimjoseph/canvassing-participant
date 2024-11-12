e29be9ba5e11a5aeec87709ac3dccde6e77aaac2
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-11-12 14:33:27 +0300

    Add [Sentry] for bug detection and crash reporting

---
+++ front-end/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://9c755de195dfff32ddd01d99e738132a@o4508253068984320.ingest.us.sentry.io/4508253070229504",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
