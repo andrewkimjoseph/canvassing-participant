7ea40d1adbeb04b7c268aab9e0cf9f05ccf4abf4
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-11-13 17:59:31 +0300

    Improve home page, and reward history

--- front-end/app/reward-history/page.tsx
+++ front-end/app/reward-history/page.tsx
@@ -8,20 +8,27 @@ import { Box, Image, Button, VStack, Text, Flex } from '@chakra-ui/react';
 import { EditIconC } from '@/components/icons/edit-icon';
 
 import { Avatar } from '@/components/ui/avatar';
+import useRewardStore from '@/stores/useRewardStore';
+import useSurveyStore from '@/stores/useSurveyStore';
 export default function RewardHistory() {
   const [userAddress, setUserAddress] = useState('');
   const [isMounted, setIsMounted] = useState(false);
   const { address, isConnected } = useAccount();
+  // const { rewards, fetchRewards } = useRewardStore();
+  const { surveys, loading, fetchSurveys } = useSurveyStore();
+  
+
+  useEffect(() => {
+    fetchSurveys();
+  }, [fetchSurveys]);
+
+  // useEffect(() => {
+  //   fetchRewards(address?.toString()!);
+  // }, [address, isConnected]);
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  useEffect(() => {
-    if (isConnected && address) {
-      setUserAddress(address);
-    }
-  }, [address, isConnected]);
 
   if (!isMounted) {
     return null;
@@ -31,6 +38,7 @@ export default function RewardHistory() {
     <Flex
       flexDirection={'column'}
       w={'100%'}
+      h={'100vh'}
       bgColor={'#ECECEC'}
       px={4}
     >
@@ -47,47 +55,74 @@ export default function RewardHistory() {
       </Flex>
 
       <Box h="100%" overflowY="auto">
-        {['1', '2', '3', '4', '5', '6', '7', '8', '9'].map((donation) => (
-          <Box
-            key={donation}
-            bgColor="white"
-            h="25"
-            w="full"
-            borderTopRightRadius={10}
-            borderBottomRightRadius={10}
-            flexDirection={'column'}
-            pb={2}
-            mb={4}
-            mt={0}
-            pt={2}
-            pl={3}
-            borderLeft={'solid'}
-            borderColor={'black'}
-            borderLeftWidth={4}
-          >
-            <Text fontSize={'lg'} mb={2} color="#363062">
-              25-10-2024 23:20
-            </Text>
+        <Box h="100%" overflowY="auto">
+          {surveys.length > 0 ? (
+            surveys.map((donation) => (
+              <Box
+                key={donation.id}
+                bgColor="white"
+                h="25"
+                w="full"
+                borderTopRightRadius={10}
+                borderBottomRightRadius={10}
+                flexDirection="column"
+                pb={2}
+                mb={4}
+                mt={0}
+                pt={2}
+                pl={3}
+                borderLeft="solid"
+                borderColor="black"
+                borderLeftWidth={4}
+              >
+                <Text fontSize="lg" mb={2} color="#363062">
+                  25-10-2024 23:20
+                </Text>
 
-            <Flex
-              flexDirection="row"
-              justifyContent={'space-between'}
-              alignItems={'center'}
+                <Flex
+                  flexDirection="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Text fontSize="sm" mb={2} color="green">
+                    Transaction Completed
+                  </Text>
+
+                  <Text fontSize="sm" mb={2} color="black" mr={2}>
+                    cUSD 0.25
+                  </Text>
+                </Flex>
+
+                <Text fontSize="lg" color="black">
+                  MiniPay
+                </Text>
+              </Box>
+            ))
+          ) : (
+            <Box
+              bgColor="gray.100"
+              h="25"
+              w="full"
+              borderRadius={10}
+              flexDirection="column"
+              pb={2}
+              mb={4}
+              mt={0}
+              pt={2}
+              pl={3}
+              border="solid"
+              borderColor="gray.300"
+              borderWidth={1}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
             >
-              <Text fontSize={'sm'} mb={2} color="green">
-                Transaction Completed
+              <Text fontSize="lg" color="gray.500">
+                No rewards available
               </Text>
-
-              <Text fontSize={'sm'} mb={2} color="black" mr={2}>
-                cUSD 0.25
-              </Text>
-            </Flex>
-
-            <Text fontSize={'lg'} color="black">
-              MiniPay
-            </Text>
-          </Box>
-        ))}
+            </Box>
+          )}
+        </Box>
       </Box>
     </Flex>
   );
