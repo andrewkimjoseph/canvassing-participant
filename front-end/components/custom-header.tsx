e3889215521ec781a06b282652d4d96b58f04c8a
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-11-13 17:56:11 +0300

    Add automatic wallet connection

--- front-end/components/custom-header.tsx
+++ front-end/components/custom-header.tsx
@@ -14,8 +14,8 @@ import {
   DrawerFooter,
   DrawerHeader,
   DrawerRoot,
-  DrawerTrigger,
   Text,
+  DrawerTrigger,
 } from '@chakra-ui/react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { Avatar } from '@/components/ui/avatar';
@@ -26,15 +26,32 @@ import { DrawerLogoC } from './drawer-logo';
 import { DrawerCardC } from './drawer-card';
 import { HomeIconC } from './icons/home-icon';
 import { RewardHistoryIconC } from './icons/reward-history-icon';
-import { ProfileIconC } from './icons/profile-icon';
 import { CloseIconC } from './icons/close-icon';
-import { EditIconC } from './icons/edit-icon';
+import useParticipantStore from '@/stores/useParticipantStore'; // Import the participant store
+import { useState, useEffect } from 'react';
+import { injected } from 'wagmi/connectors';
+import { useConnect } from 'wagmi';
 
 const CustomHeader = () => {
+  const { participant } = useParticipantStore(); // Access participant data from the store
+  const [isMiniPay, setIsMiniPay] = useState(false);
+  const { connect } = useConnect();
+
+  useEffect(() => {
+    if (
+      window.ethereum &&
+      (window.ethereum.isMiniPay || window.ethereum.isMinipay)
+    ) {
+      setIsMiniPay(true);
+      connect({ connector: injected({ target: "metaMask" }) });
+    }
+  }, []);
+
+
   return (
     <DrawerRoot size={'full'} placement={'start'}>
       <DrawerBackdrop />
-      <Box bg="#363062" px={4} position="sticky" top="0" left="0" zIndex="1000" >
+      <Box bg="#363062" px={4} position="sticky" top="0" left="0" zIndex="1000">
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <DrawerTrigger asChild>
             <IconButton size={'md'} aria-label={'Open Menu'}>
@@ -45,11 +62,7 @@ const CustomHeader = () => {
           <Link href="/">
             <LogoC />
           </Link>
-          <Circle
-            size="10px"
-            bg={`${true ? 'green' : 'red'}`}
-            color="white"
-          ></Circle>
+          <Circle size="10px" bg={`${true ? 'green' : 'red'}`} color="white" />
         </Flex>
       </Box>
 
@@ -62,7 +75,6 @@ const CustomHeader = () => {
             pt={3}
           >
             <DrawerLogoC />
-
             <DrawerActionTrigger>
               <CloseIconC />
             </DrawerActionTrigger>
@@ -80,9 +92,6 @@ const CustomHeader = () => {
                 link={'/reward-history'}
               />
             </DrawerActionTrigger>
-            {/* <DrawerActionTrigger>
-              <DrawerCardC SVGIcon={ProfileIconC} text={'Profile'} link={'/'} />
-            </DrawerActionTrigger> */}
           </Flex>
         </DrawerBody>
         <DrawerFooter flexDirection={'column'} alignItems={'center'} pb={16}>
@@ -104,22 +113,18 @@ const CustomHeader = () => {
 
             <Box className="flex flex-col items-left relative ml-4">
               <Text fontSize={18} mb={2} color={'white'}>
-                Userxxxx
+                {participant?.username || 'Userxxxx'}
               </Text>
               <Text fontSize={14} mb={2} color={'white'}>
                 Participant
               </Text>
             </Box>
-
-            {/* <Box className="flex flex-col items-left relative ml-4">
-              <EditIconC />
-            </Box> */}
           </Box>
         </DrawerFooter>
         <DrawerCloseTrigger />
       </DrawerContent>
     </DrawerRoot>
   );
-}
+};
 
 export default CustomHeader;
