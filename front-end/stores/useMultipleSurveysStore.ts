8209bb1e4042077c5c7fb20ac2f464534dcbd843
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-11-14 17:47:50 +0300

    Update store to filter unavailable surveys

--- front-end/stores/useMultipleSurveysStore.ts
+++ front-end/stores/useMultipleSurveysStore.ts
@@ -1,28 +1,45 @@
-// /stores/useSurveyStore.ts
 import { create } from 'zustand';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { Survey } from '@/entities/survey';
 import { db } from '@/firebase';
-
+import { Reward } from '@/entities/reward';
 
 interface SurveyStoreState {
   surveys: Survey[];
   loading: boolean;
-  fetchSurveys: () => Promise<void>;
+  fetchSurveys: (walletAddress: string) => Promise<void>;
 }
 
 const useMultipleSurveysStore = create<SurveyStoreState>((set) => ({
   surveys: [],
   loading: false,
-  
-  fetchSurveys: async () => {
+
+  fetchSurveys: async (walletAddress: string) => {
     set({ loading: true });
     try {
-      const snapshot = await getDocs(collection(db, 'surveys'));
-      const data = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      })) as Survey[];
+      const rewardQuery = query(
+        collection(db, 'rewards'),
+        where('participantWalletAddress', '==', walletAddress)
+      );
+      const rewardSnapshot = await getDocs(rewardQuery);
+      const participatedSurveyIds = rewardSnapshot.docs.map(
+        (doc) => (doc.data() as Reward).surveyId
+      );
+
+      let surveyQuery;
+      if (participatedSurveyIds.length > 0) {
+        surveyQuery = query(
+          collection(db, 'surveys'),
+          where('id', 'not-in', participatedSurveyIds)
+        );
+      } else {
+        surveyQuery = query(collection(db, 'surveys'));
+      }
+      const surveySnapshot = await getDocs(surveyQuery);
+      const data = surveySnapshot.docs.map((doc) => ({
+        ...(doc.data() as Survey), // Assert type here
+      }));
+
       set({ surveys: data, loading: false });
     } catch (error) {
       console.error('Error fetching surveys:', error);
