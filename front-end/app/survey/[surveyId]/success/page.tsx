72cea82cf4c138d375830edefe6f27c44ef5bd65
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-11-15 12:51:13 +0300

    [feat] Improve UI

--- front-end/app/survey/[surveyId]/success/page.tsx
+++ front-end/app/survey/[surveyId]/success/page.tsx
@@ -4,18 +4,88 @@ import { useEffect, useState } from 'react';
 
 import { useAccount } from 'wagmi';
 
-import { Box, Image, Button, Text, Flex, Spinner } from '@chakra-ui/react';
+import { Box, Image, Text, Flex, Spinner } from '@chakra-ui/react';
 
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
+import { processRewardClaimByParticipant } from '@/services/processRewardClaimByParticipant';
+import { Address } from 'viem';
+import { Toaster, toaster } from '@/components/ui/toaster';
+import useSingleSurveyStore from '@/stores/useSingleSurveyStore';
+import { StringDecoder } from 'string_decoder';
+import { Button } from '@/components/ui/button';
+import { getContractBalance } from '@/services/checkContractBalance';
+
 export default function SuccessPage() {
   const [userAddress, setUserAddress] = useState('');
   const [isMounted, setIsMounted] = useState(false);
   const { address, isConnected } = useAccount();
   const router = useRouter();
+  const { survey, fetchSurvey } = useSingleSurveyStore();
+  const [isProcessingRewardClaim, setIsProcessingRewardClaim] = useState(false);
 
+  const params = useParams();
+  const surveyId = params.surveyId;
+
+  const processRewardClaimByParticipantFn = async () => {
+    setIsProcessingRewardClaim(true);
+    if (!survey?.contractAddress || !survey?.rewardAmountIncUSD) {
+      toaster.create({
+        description: 'CE1',
+        duration: 3000,
+        type: 'error',
+      });
+      setIsProcessingRewardClaim(false);
+      return;
+    }
+
+    const contractBalance = await getContractBalance(address, {
+      _contractAddress: survey.contractAddress as Address,
+    });
+
+    if (contractBalance < survey.rewardAmountIncUSD) {
+      toaster.create({
+        description: 'CE2',
+        duration: 3000,
+        type: 'error',
+      });
+      setIsProcessingRewardClaim(false);
+      return;
+    }
+
+    const claimIsProcessed = await processRewardClaimByParticipant(address, {
+      _participantWalletAddress: address as Address,
+      _smartContractAddress: survey.contractAddress as Address,
+    });
+
+    if (claimIsProcessed) {
+      toaster.create({
+        description: 'Reward claimed successfully!',
+        duration: 3000,
+        type: 'success',
+      });
+
+      // Wait for user feedback before navigating
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      router.push(`/survey/${surveyId}/transaction-successful`);
+    } else {
+      toaster.create({
+        description: 'Failed to claim reward. Please try again.',
+        duration: 3000,
+        type: 'error',
+      });
+    }
+
+    setIsProcessingRewardClaim(false);
+  };
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  useEffect(() => {
+    if (surveyId) {
+      fetchSurvey(surveyId as string);
+    }
+  }, [surveyId, fetchSurvey]);
 
   useEffect(() => {
     if (isConnected && address) {
@@ -32,6 +102,8 @@ export default function SuccessPage() {
 
   return (
     <Flex flexDirection={'column'} w={'100%'} bgColor={'#ECECEC'} h={'100vh'}>
+      <Toaster />
+
       <Box
         bgColor="white"
         h="25"
@@ -70,7 +142,7 @@ export default function SuccessPage() {
               alignSelf={'center'}
               mt={4}
             >
-              You earned 0.25 cUSD
+              You earned {survey?.rewardAmountIncUSD} cUSD
             </Text>
 
             <Text fontSize={'sm'} mb={2} color="black" textAlign={'center'}>
@@ -80,7 +152,6 @@ export default function SuccessPage() {
           </Flex>
         </Box>
       </Box>
-
       <Button
         bgColor={'#363062'}
         borderRadius={15}
@@ -89,7 +160,9 @@ export default function SuccessPage() {
         mt={5}
         alignSelf={'center'}
         mb={16}
-        onClick={() => router.push('/transaction-successful')}
+        onClick={processRewardClaimByParticipantFn}
+        loading={isProcessingRewardClaim}
+        loadingText="Processing claim"
       >
         <Text fontSize="16" fontWeight="bold" color="white">
           Claim
