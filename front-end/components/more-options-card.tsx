5032367de515f4d9befab7895c7b05e190deccfe
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-12-05 17:09:37 +0300

    [fix] reduce font size

--- front-end/components/more-options-card.tsx
+++ front-end/components/more-options-card.tsx
@@ -36,11 +36,11 @@ export const MoreOptionsCard: React.FC<Props> = ({
         <SVGIcon />
 
         <Box className="flex flex-col " rounded={'lg'} p={4}>
-          <Text fontSize={20} pl={2} color={'black'} fontWeight={'semibold'}>
+          <Text fontSize={18} pl={2} color={'black'} fontWeight={'semibold'}>
             {primaryText}
           </Text>
 
-          <Text fontSize={16} pl={2} color={'grey'}>
+          <Text fontSize={14} pl={2} color={'grey'}>
             {secondaryText}
           </Text>
         </Box>
