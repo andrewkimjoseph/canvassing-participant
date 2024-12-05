0cbb41f4ee294b898eedc8c634a26890c191e90c
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-12-05 12:53:37 +0300

    [feat] Add "More" card

---
+++ front-end/components/more-options-card.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { IconSvgProps } from '@/types/svgIcon';
+import { Box, Text, Link } from '@chakra-ui/react';
+
+interface Props {
+  SVGIcon: React.FC<IconSvgProps>;
+  primaryText: string;
+  secondaryText: string;
+
+  link: string;
+  onClick?: () => void; // Optional onClick callback
+}
+
+export const MoreOptionsCard: React.FC<Props> = ({
+  SVGIcon,
+  primaryText,
+  secondaryText,
+  link,
+  onClick,
+}) => {
+  return (
+    <Link
+      href={link}
+      py={1}
+      onClick={onClick} // Attach the onClick callback here
+    >
+      <Box
+        className="flex flex-row items-center"
+        my={2}
+        pl={3}
+        rounded={'lg'}
+        bgColor={'white'}
+        w={'100vw'}
+      >
+        <SVGIcon />
+
+        <Box className="flex flex-col " rounded={'lg'} p={4}>
+          <Text fontSize={20} pl={2} color={'black'} fontWeight={'semibold'}>
+            {primaryText}
+          </Text>
+
+          <Text fontSize={16} pl={2} color={'grey'}>
+            {secondaryText}
+          </Text>
+        </Box>
+      </Box>
+    </Link>
+  );
+};
