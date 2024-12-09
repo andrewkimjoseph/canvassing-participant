da6b87e3f7e1399a26ed12e9e2735c127d6e4cb7
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-12-09 15:11:08 +0300

    [feat] add minipay provider

--- front-end/providers/minipay-provider.tsx
+++ front-end/providers/minipay-provider.tsx
@@ -1,31 +1,48 @@
+import { SpinnerIconC } from '@/components/icons/spinner-icon';
+import { Flex, Text } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 
 export const MiniPayProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  const [isMiniPay, setIsMiniPay] = useState(false);
+  const [isMiniPay, setIsMiniPay] = useState<boolean | null>(null);
 
   useEffect(() => {
     const checkMiniPay = () => {
       // Check for MiniPay in window.ethereum
-      const miniPayDetected = 
-        typeof window !== 'undefined' && 
-        window.ethereum && 
-        (window.ethereum.isMiniPay || window.ethereum.isMinipay);
-      
-      setIsMiniPay(!!miniPayDetected);
+      console.log(window);
+      const miniPayExists =
+        window.ethereum &&
+        (window.ethereum.isMiniPay || window.ethereum.isMinipay)
+          ? true
+          : false;
+
+      console.log(miniPayExists);
+      setIsMiniPay(miniPayExists);
     };
 
-    // Check on initial mount
     checkMiniPay();
   }, []);
 
-  // If not MiniPay, render null
-  if (!isMiniPay) {
-    return null;
+  if (isMiniPay === null) {
+    return (
+      <Flex justify="center" align="center" minH="100vh">
+        <SpinnerIconC />
+      </Flex>
+    );
   }
 
-  // Render children if MiniPay is detected
+  if (isMiniPay === false) {
+    return (
+      <Flex justify="center" align="center" minH="100vh">
+        <Text fontSize={'md'} color="#363062" textAlign={'center'}>
+          This dApp is only available on MiniPay on Opera Mini / Opera Mini
+          Beta, or the MiniPay Standalone App
+        </Text>
+      </Flex>
+    );
+  }
+
   return <>{children}</>;
 };
 
