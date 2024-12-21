3db0075c2f4ec6e99419a7bbd7fb3e8dfb589276
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-12-21 11:07:22 +0300

    add faq icon

---
+++ front-end/components/icons/more-options-icons/faqs-icons.tsx
@@ -0,0 +1,26 @@
+import { IconSvgProps } from '@/types/svgIcon';
+import * as React from 'react';
+
+export const FAQsIconC: React.FC<IconSvgProps> = ({}) => {
+  return (
+    <svg
+      width="44"
+      height="44"
+      viewBox="0 0 44 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="44" height="44" rx="22" fill="#363062" />
+      <path
+        d="M19 19L19.3353 17.9941C20.0389 15.8833 22.8236 15.4314 24.1585 17.2114C24.9405 18.254 24.886 19.7017 24.0277 20.6826L23.1085 21.7331C22.3939 22.5498 22 23.5982 22 24.6834V25M32 22C32 27.5228 27.5228 32 22 32C16.4772 32 12 27.5228 12 22C12 16.4772 16.4772 12 22 12C27.5228 12 32 16.4772 32 22Z"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M23 28C23 28.5523 22.5523 29 22 29C21.4477 29 21 28.5523 21 28C21 27.4477 21.4477 27 22 27C22.5523 27 23 27.4477 23 28Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
