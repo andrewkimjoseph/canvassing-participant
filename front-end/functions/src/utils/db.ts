cf1e0e871532cc950f218ceb3c42d3413f71c5d3
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-01-06 21:17:03 +0300

    fix: avoid multiple reward creations

--- front-end/functions/src/utils/db.ts
+++ front-end/functions/src/utils/db.ts
@@ -1,5 +1,5 @@
 import * as admin from 'firebase-admin';
-import { WebhookData } from '../types/types';
+import { Reward, WebhookData } from '../types/types';
 
 admin.initializeApp();
 
@@ -8,27 +8,50 @@ const firestore = admin.firestore();
 export const createRewardDocument = async (
   data: WebhookData,
   participantId: string,
-  walletAddress: string,
-) => {
-  const rewardDoc = firestore.collection('rewards').doc();
-  await rewardDoc.set({
-    id: rewardDoc.id,
-    surveyId: data.fields.find(field => field.label === 'surveyId')?.value,
-    participantId,
-    respondentId: data.respondentId,
-    formId: data.formId,
-    submissionId: data.submissionId,
-    isClaimed: false,
-    participantWalletAddress: walletAddress,
-    responseId: data.responseId,
-    timeCreated: admin.firestore.FieldValue.serverTimestamp(),
-    timeUpdated: null,
-    transactionHash: null,
-    amountIncUSD: null,
-  });
-  return rewardDoc.id;
-};
+  walletAddress: string
+): Promise<string> => {
+  let rewardId: string | null = null;
 
+  const existingReward = await firestore
+    .collection('rewards')
+    .where(
+      'surveyId',
+      '==',
+      data.fields.find((field) => field.label === 'surveyId')?.value
+    )
+    .where('participantId', '==', participantId)
+    .where('respondentId', '==', data.respondentId)
+    .where('formId', '==', data.formId)
+    .where('submissionId', '==', data.submissionId)
+    .get();
+
+  if (existingReward.empty) {
+    const rewardDoc = firestore.collection('rewards').doc();
+
+    await rewardDoc.set({
+      id: rewardDoc.id,
+      surveyId: data.fields.find((field) => field.label === 'surveyId')?.value,
+      participantId,
+      respondentId: data.respondentId,
+      formId: data.formId,
+      submissionId: data.submissionId,
+      isClaimed: false,
+      participantWalletAddress: walletAddress,
+      responseId: data.responseId,
+      timeCreated: admin.firestore.FieldValue.serverTimestamp(),
+      timeUpdated: null,
+      transactionHash: null,
+      amountIncUSD: null,
+    });
+
+    rewardId = rewardDoc.id;
+  } else {
+    let reward = existingReward.docs[0].data() as Reward;
+    rewardId = reward.id;
+  }
+
+  return rewardId;
+};
 
 export const updateRewardWhitelistingTransactionHash = async (
   rewardId: string,
