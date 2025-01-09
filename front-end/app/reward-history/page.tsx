66dc80be3d924b0e3ab791420b9019b958863dd8
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-01-09 16:02:10 +0300

    update: add hook dependencies

--- front-end/app/reward-history/page.tsx
+++ front-end/app/reward-history/page.tsx
@@ -26,7 +26,7 @@ export default function RewardHistory() {
       getParticipant(address);
       fetchRewards(address);
     }
-  }, [isConnected, address, getParticipant]);
+  }, [isConnected, address, getParticipant, fetchRewards]);
 
   useEffect(() => {
     checkParticipantStatus();
