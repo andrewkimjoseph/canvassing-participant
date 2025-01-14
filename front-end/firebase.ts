eddf2d1c372d591d1ea8023f4254110784e1b7b0
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-01-14 09:04:22 +0300

    revert

--- front-end/firebase.ts
+++ front-end/firebase.ts
@@ -4,7 +4,7 @@ import { Auth, getAuth, connectAuthEmulator } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: "participant-git-front-end-the-canvassings-projects.vercel.app",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
