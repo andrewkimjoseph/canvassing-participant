adc6283fde1b0a020507a3c4cac269057397e568
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-01-14 09:00:29 +0300

    hard set to front-end preview link

--- front-end/firebase.ts
+++ front-end/firebase.ts
@@ -4,7 +4,7 @@ import { Auth, getAuth, connectAuthEmulator } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  authDomain: "participant-git-front-end-the-canvassings-projects.vercel.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
