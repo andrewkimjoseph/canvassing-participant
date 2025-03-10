44aef556686de7a9778a23787b3391e6f39419b8
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-03-10 17:06:08 +0300

    chore: comment out NIG-KEN provider

--- front-end/providers/nigeria-kenya-provider.tsx
+++ front-end/providers/nigeria-kenya-provider.tsx
@@ -1,52 +1,52 @@
-import { SpinnerIconC } from '@/components/icons/spinner-icon';
-import { Flex, Text } from '@chakra-ui/react';
-import React, { useState, useEffect } from 'react';
+// import { SpinnerIconC } from '@/components/icons/spinner-icon';
+// import { Flex, Text } from '@chakra-ui/react';
+// import React, { useState, useEffect } from 'react';
 
-export const NigeriaKenyaProvider: React.FC<{
-  children: React.ReactNode;
-}> = ({ children }) => {
-  const [isAllowedCountry, setIsAllowedCountry] = useState<boolean | null>(
-    null
-  );
+// export const NigeriaKenyaProvider: React.FC<{
+//   children: React.ReactNode;
+// }> = ({ children }) => {
+//   const [isAllowedCountry, setIsAllowedCountry] = useState<boolean | null>(
+//     null
+//   );
 
-  useEffect(() => {
-    const checkCountry = async () => {
-      try {
-        const response = await fetch('https://ipapi.co/json/');
-        const data = await response.json();
+//   useEffect(() => {
+//     const checkCountry = async () => {
+//       try {
+//         const response = await fetch('https://ipapi.co/json/');
+//         const data = await response.json();
 
-        // const allowedCountries = ['NG', 'KE'];
-        const allowedCountries = ['KE'];
+//         // const allowedCountries = ['NG', 'KE'];
+//         const allowedCountries = ['KE'];
 
-        setIsAllowedCountry(allowedCountries.includes(data.country_code));
-      } catch (error) {
-        setIsAllowedCountry(false);
-      }
-    };
+//         setIsAllowedCountry(allowedCountries.includes(data.country_code));
+//       } catch (error) {
+//         setIsAllowedCountry(false);
+//       }
+//     };
 
-    checkCountry();
-  }, []);
+//     checkCountry();
+//   }, []);
 
-  if (isAllowedCountry === null) {
-    return (
-      <Flex justify="center" align="center" minH="100vh">
-        <SpinnerIconC />
-      </Flex>
-    );
-  }
+//   if (isAllowedCountry === null) {
+//     return (
+//       <Flex justify="center" align="center" minH="100%">
+//         <SpinnerIconC />
+//       </Flex>
+//     );
+//   }
 
-  if (isAllowedCountry === false) {
-    return (
-      <Flex justify="center" align="center" minH="100vh">
-        <Text fontSize={'md'} color="#363062" textAlign={'center'}>
-          This dApp is not available in your country.
-        </Text>
-      </Flex>
-    );
-  }
+//   if (isAllowedCountry === false) {
+//     return (
+//       <Flex justify="center" align="center" minH="100%">
+//         <Text fontSize={'md'} color="#363062" textAlign={'center'}>
+//           This dApp is not available in your country.
+//         </Text>
+//       </Flex>
+//     );
+//   }
 
-  // Render children if from allowed countries
-  return <>{children}</>;
-};
+//   // Render children if from allowed countries
+//   return <>{children}</>;
+// };
 
-export default NigeriaKenyaProvider;
+// export default NigeriaKenyaProvider;
