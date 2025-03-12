f826d54369f4a6df4f6f20e0f42de5d570aa60b5
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-03-12 13:01:57 +0300

    update: filter cUSD-rewarded surveys and G$-rewarded surveys

--- front-end/entities/survey.ts
+++ front-end/entities/survey.ts
@@ -1,24 +1,23 @@
-import { RewardToken } from '@/types/rewardToken';
-import * as admin from 'firebase-admin';
-import { Address } from 'viem';
-
+import { RewardToken } from "@/types/rewardToken";
+import * as admin from "firebase-admin";
+import { Address } from "viem";
 
 export interface Survey {
-    id: string;
-    researcherId: string | null;
-    topic: string | null;
-    brief: string | null;
-    instructions: string | null;
-    durationInMinutes: number | null;
-    formLink: string | null;
-    rewardAmountIncUSD: number | null;
-    contractAddress: Address | null;
-    isAvailable: boolean | null;
-    timeCreated: admin.firestore.Timestamp | null;
-    targetCountry: string | null;
-    targetGender: string | null;
-    isAlreadyBookedByUser: boolean | null;
-    isTest: boolean | null;
-    isForTestnet: boolean | null;
-    rewardToken: RewardToken  | null;
-  }
+  id: string;
+  researcherId: string | null;
+  topic: string | null;
+  brief: string | null;
+  instructions: string | null;
+  durationInMinutes: number | null;
+  formLink: string | null;
+  rewardAmountIncUSD: number | null;
+  contractAddress: Address | null;
+  isAvailable: boolean | null;
+  timeCreated: admin.firestore.Timestamp | null;
+  targetCountry: string | null;
+  targetGender: string | null;
+  isAlreadyBookedByUser: boolean | null;
+  isTest: boolean | null;
+  isForTestnet: boolean | null;
+  rewardToken: RewardToken | null;
+}
---
+++ front-end/utils/addresses/tokens.ts
@@ -0,0 +1,9 @@
+export const cUSDAlfajoresContractAddress: `0x${string}` =
+"0x874069fa1eb16d44d622f2e0ca25eea172369bc1";
+
+
+export const cUSDMainnetContractAddress: `0x${string}` =
+"0x765DE816845861e75A25fCA122bb6898B8B1282a";
+
+export const goodDollarMainnetContractAddress: `0x${string}` =
+"0x62B8B11039FcfE5aB0C56E502b1C372A3d2a9c7A";
