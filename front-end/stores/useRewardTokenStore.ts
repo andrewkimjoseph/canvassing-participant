335d78444b4e7d34155c5e8b4089fc77d896b505
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-03-12 09:11:50 +0300

    update: add useRewardTokenStore.ts and rewardToken.ts

---
+++ front-end/stores/useRewardTokenStore.ts
@@ -0,0 +1,24 @@
+import { RewardToken } from "@/types/rewardToken";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+
+
+interface RewardTokenStoreState {
+  currentToken: RewardToken;
+  setCurrentToken: (token: RewardToken) => void;
+}
+
+const useRewardTokenStore = create<RewardTokenStoreState>()(
+  persist(
+    (set) => ({
+      currentToken: RewardToken.celoDollar, // Default value
+      setCurrentToken: (token: RewardToken) => set({ currentToken: token }),
+    }),
+    {
+      name: "reward-token-storage",
+    }
+  )
+);
+
+export default useRewardTokenStore;
