4e0771c7e43674ec6245d800edf93c1ba1b23c08
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-03-12 14:36:14 +0300

    add comment

--- front-end/types/rewardToken.ts
+++ front-end/types/rewardToken.ts
@@ -1,5 +1,13 @@
-// Enum for reward token types
+/**
+ * Represents the types of tokens that can be used as rewards in the system.
+ * 
+ * celoDollar: Represents the Celo Dollar (cUSD) stablecoin on the Celo network.
+ *             Used primarily for mainnet rewards. Value pegged to the US Dollar.
+ * 
+ * goodDollar: Represents the GoodDollar (G$) token, a universal basic income token
+ *             on the Celo network. Used for social impact initiatives.
+ */
 export enum RewardToken {
-    celoDollar = "celoDollar",
-    goodDollar = "goodDollar"
-  }
+  celoDollar = "celoDollar",
+  goodDollar = "goodDollar"
+}
