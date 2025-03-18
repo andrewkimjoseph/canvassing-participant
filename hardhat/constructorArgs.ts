67e492030aeaecd81e0a76dd1481ca512db3f3bd
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-03-18 08:56:33 +0300

    update constructorArgs.ts

--- hardhat/constructorArgs.ts
+++ hardhat/constructorArgs.ts
@@ -3,8 +3,8 @@ import { Address, parseEther } from "viem";
 
 const constructorArgs: [Address, BigInt, BigInt, Address] = [
   "0xE49B05F2c7DD51f61E415E1DFAc10B80074B001A",
-  parseEther("0.25"),
-  BigInt(1),
+  parseEther("0.05"),
+  BigInt(5),
   "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1",
 ];
 
