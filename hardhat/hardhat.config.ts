b181111a27d1e4c60e021ed234d813d1c73f7a33
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-03-18 11:24:17 +0300

    update config with correct .env variables

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,30 +1,27 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-verify";
-import { mnemonicToAccount } from "viem/accounts";
-import { toHex } from "viem";
+import { Address } from "viem";
 import { vars } from "hardhat/config";
 
-const SRP = vars.get("SRP");
+const PK_ONE = vars.get("SRP");
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 
 import * as dotenv from "dotenv";
 
-// Configure dotenv to load the .env file
 dotenv.config();
 
-
-if (!SRP) throw new Error("SRP not found in environment variables");
+if (!PK_ONE) throw new Error("SRP not found in environment variables");
 if (!INFURA_API_KEY)
   throw new Error("INFURA_API_KEY not found in environment variables");
 
 const INFURA_RPC_URL = `https://celo-alfajores.infura.io/v3/${INFURA_API_KEY}`;
 
-const PRIVATE_KEY = toHex(mnemonicToAccount(SRP).getHdKey().privateKey as Uint8Array);
+const PK = `0x${process.env.PK_ONE}` as Address;
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.28",
+    version: "0.8.29",
     settings: {
       optimizer: {
         enabled: true,
@@ -35,16 +32,16 @@ const config: HardhatUserConfig = {
   networks: {
     celoAlfajores: {
       url: INFURA_RPC_URL,
-      accounts: [PRIVATE_KEY],
+      accounts: [PK],
       chainId: 44787,
-    }
+    },
   },
   sourcify: {
-    enabled: true
+    enabled: true,
   },
   etherscan: {
     apiKey: {
-      celoAlfajores: "no-api-key-needed"
+      celoAlfajores: "no-api-key-needed",
     },
     customChains: [
       {
@@ -52,10 +49,10 @@ const config: HardhatUserConfig = {
         chainId: 44787,
         urls: {
           apiURL: "https://celo-alfajores.blockscout.com/api",
-          browserURL: "https://celo-alfajores.blockscout.com"
-        }
-      }
-    ]
+          browserURL: "https://celo-alfajores.blockscout.com",
+        },
+      },
+    ],
   },
 };
 
