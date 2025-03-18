5b4077f3f44f3675653654f9ec954f6ef83768a8
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-03-18 08:56:23 +0300

    update contract ignition module

--- hardhat/ignition/modules/ClosedSurveyV6.ts
+++ hardhat/ignition/modules/ClosedSurveyV6.ts
@@ -2,12 +2,12 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { parseEther } from "viem";
 
 const RESEARCHER_WALLET_ADDRESS = "0xE49B05F2c7DD51f61E415E1DFAc10B80074B001A";
-const _REWARD_AMOUNT_PER_PARTICIPANT_IN_WEI = parseEther("0.25");
-const _TARGET_NUMBER_OF_PARTICIPANTS = BigInt(1);
+const _REWARD_AMOUNT_PER_PARTICIPANT_IN_WEI = parseEther("0.05");
+const _TARGET_NUMBER_OF_PARTICIPANTS = BigInt(5);
 const CUSD_TOKEN_ADDRESS = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
 
-const CanvassingSurveyModule = buildModule("CanvassingSurveyModule", (imb) => {
+const ClosedSurveyV6Module = buildModule("ClosedSurveyV6Module", (imb) => {
   const researcherWalletAddress = imb.getParameter(
     "researcherWalletAddress",
     RESEARCHER_WALLET_ADDRESS
@@ -28,14 +28,15 @@ const CanvassingSurveyModule = buildModule("CanvassingSurveyModule", (imb) => {
     CUSD_TOKEN_ADDRESS
   );
 
-  const canvassingSurvey = imb.contract("CanvassingSurvey", [
+  // Updated to use the fully qualified contract name
+  const closedSurveyV6 = imb.contract("contracts/ClosedSurveyV6.sol:ClosedSurveyV6", [
     researcherWalletAddress,
     _rewardAmountPerParticipantInWei,
     _targetNumberOfParticipants,
     cUSDTokenAddress,
   ]);
 
-  return { canvassingSurvey };
+  return { closedSurveyV6 };
 });
 
-export default CanvassingSurveyModule;
+export default ClosedSurveyV6Module;
