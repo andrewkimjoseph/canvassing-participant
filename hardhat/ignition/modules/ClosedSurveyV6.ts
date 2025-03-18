42ffe214900872f209b02dc954ea4798ab61e69b
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-03-18 11:24:32 +0300

    improve ignition module

--- hardhat/ignition/modules/ClosedSurveyV6.ts
+++ hardhat/ignition/modules/ClosedSurveyV6.ts
@@ -1,39 +1,38 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { parseEther } from "viem";
+import constructorArgs from "../../constructorArgs";
 
-const RESEARCHER_WALLET_ADDRESS = "0xE49B05F2c7DD51f61E415E1DFAc10B80074B001A";
-const _REWARD_AMOUNT_PER_PARTICIPANT_IN_WEI = parseEther("0.05");
-const _TARGET_NUMBER_OF_PARTICIPANTS = BigInt(5);
-const CUSD_TOKEN_ADDRESS = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
+const RESEARCHER_WALLET_ADDRESS = constructorArgs[0];
+const _REWARD_AMOUNT_PER_PARTICIPANT_IN_WEI = constructorArgs[1];
+const _TARGET_NUMBER_OF_PARTICIPANTS = constructorArgs[2];
+const _REWARDTOKEN = constructorArgs[3];
 
 
 const ClosedSurveyV6Module = buildModule("ClosedSurveyV6Module", (imb) => {
-  const researcherWalletAddress = imb.getParameter(
-    "researcherWalletAddress",
+  const researcher = imb.getParameter(
+    "researcher",
     RESEARCHER_WALLET_ADDRESS
   );
 
   const _rewardAmountPerParticipantInWei = imb.getParameter(
     "_rewardAmountPerParticipantInWei",
-    _REWARD_AMOUNT_PER_PARTICIPANT_IN_WEI
+    String(_REWARD_AMOUNT_PER_PARTICIPANT_IN_WEI)
   );
 
   const _targetNumberOfParticipants = imb.getParameter(
     "_targetNumberOfParticipants",
-    _TARGET_NUMBER_OF_PARTICIPANTS
+    String(_TARGET_NUMBER_OF_PARTICIPANTS)
   );
 
-  const cUSDTokenAddress = imb.getParameter(
-    "cUSDTokenAddress",
-    CUSD_TOKEN_ADDRESS
+  const _rewardToken = imb.getParameter(
+    "_rewardToken",
+    _REWARDTOKEN
   );
 
-  // Updated to use the fully qualified contract name
   const closedSurveyV6 = imb.contract("contracts/ClosedSurveyV6.sol:ClosedSurveyV6", [
-    researcherWalletAddress,
+    researcher,
     _rewardAmountPerParticipantInWei,
     _targetNumberOfParticipants,
-    cUSDTokenAddress,
+    _rewardToken,
   ]);
 
   return { closedSurveyV6 };
